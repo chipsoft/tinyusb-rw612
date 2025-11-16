35c7bc472a42587bf2abc1c0393fa4cd01d928fe
Denis Suprunenko <chipsoft@gmail.com>
2025-11-16 22:46:47 +0100

    fix(rw612): Configure SDK paths to use mcux-sdk and CMSIS_5
    - Update family.mk to use SDK from hw/mcu/nxp/mcux-sdk instead of parent directory
    - Add correct driver subdirectory paths (lpc_gpio, flexcomm/usart, flexspi, cache64)
    - Disable ELS/PKC crypto library (optional feature with complex dependencies)
    - Comment out glitch detector/sensor config in board.c that requires ELS
    - Resolves build error: fsl_device_registers.h not found

--- hw/bsp/rw612/family.mk
+++ hw/bsp/rw612/family.mk
@@ -2,17 +2,17 @@ UF2_FAMILY_ID = 0x2abc77ec
 JLINK_DEVICE = RW612
 JLINK_IF = swd
 
-# Use SDK from parent project (frdmrw612_freertos_hello root)
-# TOP points to app_libs/tinyusb, so $(TOP)/../.. is the repo root
-PROJECT_ROOT = $(TOP)/../..
-SDK_DRIVERS = $(PROJECT_ROOT)/drivers
-SDK_DEVICE = $(PROJECT_ROOT)/device
-SDK_CMSIS = $(PROJECT_ROOT)/CMSIS
-SDK_STARTUP = $(PROJECT_ROOT)/startup
-SDK_UTILITIES = $(PROJECT_ROOT)/utilities/debug_console_lite
-SDK_UTILITIES_STR = $(PROJECT_ROOT)/utilities/str
-SDK_COMPONENT = $(PROJECT_ROOT)/component/uart
-SDK_COMPONENT_ELS = $(PROJECT_ROOT)/component/els_pkc
+# Use SDK from hw/mcu/nxp/mcux-sdk and lib/CMSIS_5
+SDK_DIR = $(TOP)/hw/mcu/nxp/mcux-sdk
+MCU_DIR = $(SDK_DIR)/devices/RW612
+SDK_DRIVERS = $(SDK_DIR)/drivers
+SDK_DEVICE = $(MCU_DIR)
+SDK_CMSIS = $(TOP)/lib/CMSIS_5/CMSIS/Core/Include
+SDK_STARTUP = $(MCU_DIR)
+SDK_UTILITIES = $(SDK_DIR)/components/uart
+SDK_UTILITIES_STR = $(SDK_DIR)/utilities/str
+SDK_COMPONENT = $(SDK_DIR)/components/uart
+SDK_COMPONENT_ELS = $(SDK_DIR)/components/els_pkc
 
 include $(TOP)/$(BOARD_PATH)/board.mk
 
@@ -36,54 +36,58 @@ LD_FILE ?= hw/bsp/rw612/boards/frdm_rw612/build_main.ld
 # TinyUSB: RW612 uses ChipIdea HS controller
 SRC_C += src/portable/chipidea/ci_hs/dcd_ci_hs.c
 
-# RW612 SDK drivers
+# RW612 SDK drivers  
 SRC_C += \
 	$(SDK_DEVICE)/system_RW612.c \
-	$(SDK_DRIVERS)/fsl_clock.c \
-	$(SDK_DRIVERS)/fsl_reset.c \
-	$(SDK_DRIVERS)/fsl_gpio.c \
-	$(SDK_DRIVERS)/fsl_usart.c \
-	$(SDK_DRIVERS)/fsl_common.c \
-	$(SDK_DRIVERS)/fsl_common_arm.c \
-	$(SDK_DRIVERS)/fsl_power.c \
-	$(SDK_DRIVERS)/fsl_flexcomm.c \
-	$(SDK_DRIVERS)/fsl_ocotp.c \
-	$(PROJECT_ROOT)/flash_config/flash_config.c
+	$(SDK_DEVICE)/drivers/fsl_clock.c \
+	$(SDK_DEVICE)/drivers/fsl_power.c \
+	$(SDK_DEVICE)/drivers/fsl_reset.c \
+	$(SDK_DEVICE)/drivers/fsl_ocotp.c \
+	$(SDK_DRIVERS)/lpc_gpio/fsl_gpio.c \
+	$(SDK_DRIVERS)/flexcomm/fsl_flexcomm.c \
+	$(SDK_DRIVERS)/flexcomm/usart/fsl_usart.c \
+	$(SDK_DRIVERS)/common/fsl_common.c \
+	$(SDK_DRIVERS)/common/fsl_common_arm.c
 
-# ELS/PKC crypto library (for GDET and voltage calibration)
-SRC_C += \
-	$(SDK_COMPONENT_ELS)/src/comps/mcuxClEls/src/mcuxClEls_Common.c \
-	$(SDK_COMPONENT_ELS)/src/comps/mcuxClEls/src/mcuxClEls_GlitchDetector.c
+# Note: ELS/PKC crypto library disabled - missing platform headers
+# SRC_C += \
+#	$(SDK_COMPONENT_ELS)/src/comps/mcuxClEls/src/mcuxClEls_Common.c \
+#	$(SDK_COMPONENT_ELS)/src/comps/mcuxClEls/src/mcuxClEls_GlitchDetector.c
 
 INC += \
 	$(TOP)/$(BOARD_PATH) \
 	$(SDK_CMSIS) \
 	$(TOP)/hw/bsp/rw612 \
-	$(PROJECT_ROOT) \
 	$(SDK_DEVICE) \
-	$(SDK_DEVICE)/periph \
-	$(SDK_DRIVERS) \
+	$(SDK_DEVICE)/drivers \
+	$(SDK_DRIVERS)/lpc_gpio \
+	$(SDK_DRIVERS)/flexcomm \
+	$(SDK_DRIVERS)/flexcomm/usart \
+	$(SDK_DRIVERS)/common \
+	$(SDK_DRIVERS)/flexspi \
+	$(SDK_DRIVERS)/cache/cache64 \
 	$(SDK_UTILITIES) \
 	$(SDK_UTILITIES_STR) \
-	$(SDK_COMPONENT) \
-	$(PROJECT_ROOT)/flash_config \
-	$(SDK_COMPONENT_ELS)/src/comps/mcuxClBuffer/inc \
-	$(SDK_COMPONENT_ELS)/src/comps/mcuxClBuffer/inc/internal \
-	$(SDK_COMPONENT_ELS)/src/comps/mcuxClCore/inc \
-	$(SDK_COMPONENT_ELS)/src/comps/mcuxClEls/inc \
-	$(SDK_COMPONENT_ELS)/src/comps/mcuxClEls/inc/internal \
-	$(SDK_COMPONENT_ELS)/src/comps/mcuxClMemory/inc \
-	$(SDK_COMPONENT_ELS)/src/comps/mcuxClMemory/inc/internal \
-	$(SDK_COMPONENT_ELS)/src/comps/mcuxCsslMemory/inc \
-	$(SDK_COMPONENT_ELS)/src/comps/mcuxCsslMemory/inc/internal \
-	$(SDK_COMPONENT_ELS)/src/comps/mcuxCsslCPreProcessor/inc \
-	$(SDK_COMPONENT_ELS)/src/comps/mcuxCsslDataIntegrity/inc \
-	$(SDK_COMPONENT_ELS)/src/comps/mcuxCsslFlowProtection/inc \
-	$(SDK_COMPONENT_ELS)/src/comps/mcuxCsslParamIntegrity/inc \
-	$(SDK_COMPONENT_ELS)/src/comps/mcuxCsslSecureCounter/inc \
-	$(SDK_COMPONENT_ELS)/src/platforms/rw61x \
-	$(SDK_COMPONENT_ELS)/src/platforms/rw61x/inc \
-	$(SDK_COMPONENT_ELS)/includes/platform/rw61x
+	$(SDK_COMPONENT)
+
+# Note: ELS/PKC include paths disabled - missing platform headers
+#	$(SDK_COMPONENT_ELS)/src/comps/mcuxClBuffer/inc \
+#	$(SDK_COMPONENT_ELS)/src/comps/mcuxClBuffer/inc/internal \
+#	$(SDK_COMPONENT_ELS)/src/comps/mcuxClCore/inc \
+#	$(SDK_COMPONENT_ELS)/src/comps/mcuxClEls/inc \
+#	$(SDK_COMPONENT_ELS)/src/comps/mcuxClEls/inc/internal \
+#	$(SDK_COMPONENT_ELS)/src/comps/mcuxClMemory/inc \
+#	$(SDK_COMPONENT_ELS)/src/comps/mcuxClMemory/inc/internal \
+#	$(SDK_COMPONENT_ELS)/src/comps/mcuxCsslMemory/inc \
+#	$(SDK_COMPONENT_ELS)/src/comps/mcuxCsslMemory/inc/internal \
+#	$(SDK_COMPONENT_ELS)/src/comps/mcuxCsslCPreProcessor/inc \
+#	$(SDK_COMPONENT_ELS)/src/comps/mcuxCsslDataIntegrity/inc \
+#	$(SDK_COMPONENT_ELS)/src/comps/mcuxCsslFlowProtection/inc \
+#	$(SDK_COMPONENT_ELS)/src/comps/mcuxCsslParamIntegrity/inc \
+#	$(SDK_COMPONENT_ELS)/src/comps/mcuxCsslSecureCounter/inc \
+#	$(SDK_COMPONENT_ELS)/src/platforms/rw61x \
+#	$(SDK_COMPONENT_ELS)/src/platforms/rw61x/inc \
+#	$(SDK_COMPONENT_ELS)/includes/platform/rw61x
 
 # Startup file (C-based startup for RW612)
-SRC_C += $(SDK_STARTUP)/startup_rw612.c
+SRC_C += $(SDK_STARTUP)/mcuxpresso/startup_rw612.c
